6b014b28d32301862629667557a8b04bf214693c
Joseph Scott <j.scott@resilientplc.com>
2023-12-21 12:14:05 +0000

    added container to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
+import { Container } from '@mui/material';
+
 import SummitAppBar from './components/app-bar/app-bar';
 import { useAuthContext } from './providers/auth-provider/auth-context';
 
 const App: React.FC = () => {
   const { isLoading } = useAuthContext();
 
-  return isLoading ? (
-    <h1>Loading...</h1>
-  ) : (
+  return (
     <>
       <SummitAppBar />
-      <h1>Summit App</h1>
+      <Container>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <h1>Summit App</h1>
+          </>
+        )}
+      </Container>
     </>
   );
 };
