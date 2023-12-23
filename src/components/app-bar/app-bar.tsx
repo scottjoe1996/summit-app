6d93b9b5fed866a77cdd1c4a2c499d4cf66cb743
Joseph Scott <j.scott@resilientplc.com>
2023-12-23 14:57:23 +0000

    moved drawer to page component

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -1,62 +1,33 @@
 import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import { AppBar, Drawer, Grid, Toolbar, Typography } from '@mui/material';
-import { Groups3 } from '@mui/icons-material';
+import { AppBar, Grid, Toolbar, Typography } from '@mui/material';
 
 import ProfileButton from './profile-button';
 import NavigationButton from './navigation/navigation-button';
-import DrawerLinks, { DrawerLink } from './navigation/drawer-links';
-import { UserSession, useAuthContext } from '../../providers/auth-provider/auth-context';
-
-const getAvailableLinks = (userSession: UserSession): DrawerLink[] => {
-  const links: DrawerLink[] = [];
-
-  if (userSession.isAuthenticated) {
-    links.push({ icon: <Groups3 />, label: 'Schools', path: '/schools' });
-  }
-
-  return links;
-};
+import { DrawerLink } from './navigation/drawer-links';
 
 interface SummitAppBarProps {
   title: string;
+  links: DrawerLink[];
+  onOpenDrawerClick: () => void;
+  onLinkClick: (path: string) => void;
 }
 
-const SummitAppBar: React.FC<SummitAppBarProps> = ({ title }) => {
-  const [openDrawer, setOpenDrawer] = React.useState(false);
-
-  const { userSession } = useAuthContext();
-  const navigate = useNavigate();
-
-  const handleLinkClick = React.useCallback(
-    (link: string) => {
-      navigate(link);
-    },
-    [navigate]
-  );
-
-  const links = getAvailableLinks(userSession);
-
+const SummitAppBar: React.FC<SummitAppBarProps> = ({ title, links, onOpenDrawerClick, onLinkClick }) => {
   return (
-    <>
-      <AppBar position='sticky'>
-        <Toolbar>
-          <Grid container justifyContent='space-between' alignItems='center'>
-            <Grid container item xs={3} alignItems='center'>
-              <NavigationButton links={links} onOpenDrawerClick={() => setOpenDrawer(true)} onLinkClick={handleLinkClick} />
-              <Typography variant='h6' marginLeft={1}>
-                {title}
-              </Typography>
-            </Grid>
-            <ProfileButton />
+    <AppBar position='sticky'>
+      <Toolbar>
+        <Grid container justifyContent='space-between' alignItems='center'>
+          <Grid container item xs={3} alignItems='center'>
+            <NavigationButton links={links} onOpenDrawerClick={onOpenDrawerClick} onLinkClick={onLinkClick} />
+            <Typography variant='h6' marginLeft={1}>
+              {title}
+            </Typography>
           </Grid>
-        </Toolbar>
-      </AppBar>
-      <Drawer anchor='left' open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <DrawerLinks links={links} onLinkClick={handleLinkClick} />
-      </Drawer>
-    </>
+          <ProfileButton />
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Container, Drawer } from '@mui/material';
+import { Groups3 } from '@mui/icons-material';
+
 import SummitAppBar from '../components/app-bar/app-bar';
-import { Container } from '@mui/material';
+import { UserSession, useAuthContext } from '../providers/auth-provider/auth-context';
+import DrawerLinks, { DrawerLink } from '../components/app-bar/navigation/drawer-links';
+
+const getAvailableLinks = (userSession: UserSession): DrawerLink[] => {
+  const links: DrawerLink[] = [];
+
+  if (userSession.isAuthenticated) {
+    links.push({ icon: <Groups3 />, label: 'Schools', path: '/schools' });
+  }
+
+  return links;
+};
 
 interface PageProps {
   title: string;
@@ -8,9 +24,26 @@ interface PageProps {
 }
 
 const Page: React.FC<PageProps> = ({ title, content }) => {
+  const [openDrawer, setOpenDrawer] = React.useState(false);
+
+  const { userSession } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleLinkClick = React.useCallback(
+    (link: string) => {
+      navigate(link);
+    },
+    [navigate]
+  );
+
+  const links = getAvailableLinks(userSession);
+
   return (
     <>
-      <SummitAppBar title={title} />
+      <Drawer anchor='left' open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <DrawerLinks links={links} onLinkClick={handleLinkClick} />
+      </Drawer>
+      <SummitAppBar links={links} title={title} onLinkClick={handleLinkClick} onOpenDrawerClick={() => setOpenDrawer(true)} />
       <Container sx={{ paddingTop: 2 }}>{content}</Container>
     </>
   );
