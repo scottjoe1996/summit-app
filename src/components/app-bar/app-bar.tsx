89219714de4e16526a66c0be831f07400ed79398
Joseph Scott <j.scott@resilientplc.com>
2023-12-23 14:35:49 +0000

    moved drawer to app bar component

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -1,29 +1,62 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { AppBar, Grid, Toolbar, Typography } from '@mui/material';
+import { AppBar, Drawer, Grid, Toolbar, Typography } from '@mui/material';
+import { Groups3 } from '@mui/icons-material';
 
 import ProfileButton from './profile-button';
 import NavigationButton from './navigation/navigation-button';
+import DrawerLinks, { DrawerLink } from './navigation/drawer-links';
+import { UserSession, useAuthContext } from '../../providers/auth-provider/auth-context';
+
+const getAvailableLinks = (userSession: UserSession): DrawerLink[] => {
+  const links: DrawerLink[] = [];
+
+  if (userSession.isAuthenticated) {
+    links.push({ icon: <Groups3 />, label: 'Schools', path: '/schools' });
+  }
+
+  return links;
+};
 
 interface SummitAppBarProps {
   title: string;
 }
 
 const SummitAppBar: React.FC<SummitAppBarProps> = ({ title }) => {
+  const [openDrawer, setOpenDrawer] = React.useState(false);
+
+  const { userSession } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleLinkClick = React.useCallback(
+    (link: string) => {
+      navigate(link);
+    },
+    [navigate]
+  );
+
+  const links = getAvailableLinks(userSession);
+
   return (
-    <AppBar position='sticky'>
-      <Toolbar>
-        <Grid container justifyContent='space-between' alignItems='center'>
-          <Grid container item xs={3} alignItems='center'>
-            <NavigationButton />
-            <Typography variant='h6' marginLeft={1}>
-              {title}
-            </Typography>
+    <>
+      <AppBar position='sticky'>
+        <Toolbar>
+          <Grid container justifyContent='space-between' alignItems='center'>
+            <Grid container item xs={3} alignItems='center'>
+              <NavigationButton links={links} onOpenDrawerClick={() => setOpenDrawer(true)} onLinkClick={handleLinkClick} />
+              <Typography variant='h6' marginLeft={1}>
+                {title}
+              </Typography>
+            </Grid>
+            <ProfileButton />
           </Grid>
-          <ProfileButton />
-        </Grid>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor='left' open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <DrawerLinks links={links} onLinkClick={handleLinkClick} />
+      </Drawer>
+    </>
   );
 };
 
--- src/components/app-bar/navigation/navigation-button.tsx
+++ src/components/app-bar/navigation/navigation-button.tsx
@@ -1,49 +1,26 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import { Drawer, IconButton } from '@mui/material';
-import { Groups3, Menu, Terrain } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { Menu, Terrain } from '@mui/icons-material';
 
-import { UserSession, useAuthContext } from '../../../providers/auth-provider/auth-context';
-import DrawerLinks, { DrawerLink } from './drawer-links';
+import { DrawerLink } from './drawer-links';
 
-const getAvailableLinks = (userSession: UserSession): DrawerLink[] => {
-  const links: DrawerLink[] = [];
+interface NavigationButton {
+  links: DrawerLink[];
+  onLinkClick: (path: string) => void;
+  onOpenDrawerClick: () => void;
+}
 
-  if (userSession.isAuthenticated) {
-    links.push({ icon: <Groups3 />, label: 'Schools', path: '/schools' });
-  }
-
-  return links;
-};
-
-const NavigationButton: React.FC = () => {
-  const [open, setOpen] = React.useState(false);
-
-  const { userSession } = useAuthContext();
-  const navigate = useNavigate();
-
-  const handleLinkClick = React.useCallback(
-    (link: string) => {
-      navigate(link);
-    },
-    [navigate]
-  );
-
-  const links = getAvailableLinks(userSession);
-
+const NavigationButton: React.FC<NavigationButton> = ({ links, onLinkClick, onOpenDrawerClick }) => {
   return links.length === 0 ? (
-    <IconButton color='inherit' onClick={() => handleLinkClick('/')}>
+    <IconButton color='inherit' onClick={() => onLinkClick('/')}>
       <Terrain />
     </IconButton>
   ) : (
     <>
-      <IconButton color='inherit' onClick={() => setOpen(true)}>
+      <IconButton color='inherit' onClick={onOpenDrawerClick}>
         <Menu />
       </IconButton>
-      <Drawer anchor='left' open={open} onClose={() => setOpen(false)}>
-        <DrawerLinks links={links} onLinkClick={handleLinkClick} />
-      </Drawer>
     </>
   );
 };
