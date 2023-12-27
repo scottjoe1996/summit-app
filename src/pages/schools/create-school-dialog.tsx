0c7bab30662ceb33340b7d44902f49d220bef4b8
Joseph Scott <j.scott@resilientplc.com>
2023-12-27 15:13:13 +0000

    added validation for name field

--- src/pages/schools/create-school-dialog.tsx
+++ src/pages/schools/create-school-dialog.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Dialog, DialogContent, DialogTitle, TextField } from '@mui/material';
 
+import { NO_ERROR, hasError } from './form-utils';
+
 interface CreateSchoolDialogProps {
   open: boolean;
   onClose: () => void;
@@ -9,8 +11,10 @@ interface CreateSchoolDialogProps {
 
 const CreateSchoolDialog: React.FC<CreateSchoolDialogProps> = ({ open, onClose }) => {
   const [name, setName] = React.useState('');
+  const [nameError, setNameError] = React.useState(NO_ERROR);
 
   const handleSchoolNameChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setNameError(getNameError(event.target.value));
     setName(event.target.value);
   }, []);
 
@@ -18,11 +22,37 @@ const CreateSchoolDialog: React.FC<CreateSchoolDialogProps> = ({ open, onClose }
     <Dialog open={open} onClose={onClose} fullWidth>
       <DialogTitle>Create school</DialogTitle>
       <DialogContent>
-        <TextField label='Name' placeholder='The name of the school' fullWidth value={name} onChange={handleSchoolNameChange} variant='filled' />
+        <TextField
+          label='Name'
+          placeholder='The name of the school'
+          fullWidth
+          value={name}
+          onChange={handleSchoolNameChange}
+          helperText={nameError}
+          variant='filled'
+          error={hasError(nameError)}
+        />
       </DialogContent>
-      {/* TODO: School name validation, Add players (with validation), create button (with error handling)*/}
+      {/* TODO: Add admin, Add players (with validation), create button (with error handling)*/}
     </Dialog>
   );
+};
+
+const getNameError = (name: string): string => {
+  if (name.startsWith(' ')) {
+    return 'School name cannot start with an empty space';
+  }
+
+  if (name.trim().length === 0) {
+    return 'School name cannot be empty';
+  }
+
+  const maxLength = 64;
+  if (name.length > maxLength) {
+    return `School name cannot have more than ${maxLength} characters`;
+  }
+
+  return NO_ERROR;
 };
 
 export default CreateSchoolDialog;
---
+++ src/pages/schools/form-utils.ts
@@ -0,0 +1,3 @@
+export const NO_ERROR = ' ';
+
+export const hasError = (errorString: string): boolean => errorString !== NO_ERROR;
