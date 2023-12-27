8aa461a090300af68c793718d89af19b782bde82
Joseph Scott <j.scott@resilientplc.com>
2023-12-27 10:24:45 +0000

    added create school button with dummy dialog

--- src/pages/schools/schools-page.tsx
+++ src/pages/schools/schools-page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Button, Dialog, DialogTitle, Grid } from '@mui/material';
+
 import { School, SchoolsApi } from '../../apis/schools-api';
 
 import SchoolsTable from './schools-table';
@@ -7,6 +9,7 @@ import SchoolsTable from './schools-table';
 const schoolsApi = new SchoolsApi();
 
 const SchoolsPage: React.FC = () => {
+  const [openSchoolDialog, setOpenSchoolDialog] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
   const [hasError, setHasError] = React.useState(false);
   const [schools, setSchools] = React.useState<School[]>([]);
@@ -31,7 +34,22 @@ const SchoolsPage: React.FC = () => {
     fetchSchools();
   }, [fetchSchools]);
 
-  return <SchoolsTable schools={schools} loading={loading} hasError={hasError} onRetry={fetchSchools} />;
+  return (
+    <>
+      {!hasError && !loading && (
+        <Grid paddingBottom={1} container justifyContent='flex-end'>
+          <Button variant='contained' onClick={() => setOpenSchoolDialog(true)}>
+            Create school
+          </Button>
+        </Grid>
+      )}
+      <SchoolsTable schools={schools} loading={loading} hasError={hasError} onRetry={fetchSchools} />
+      <Dialog open={openSchoolDialog} onClose={() => setOpenSchoolDialog(false)}>
+        <DialogTitle>Create school</DialogTitle>
+        {/* TODO */}
+      </Dialog>
+    </>
+  );
 };
 
 export default SchoolsPage;
